e979ea318f7ec463b78065bd47850e178659276d
Matias Fernandez Martinez <thematho@gmail.com>
2018-10-12 00:37:22 -0300

    Fixed uglify plugin in production webpack Fix merge issues on package JSON

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
-var path    = require('path');
-var config  = require('../webpack.config');
+var path = require('path');
+var config = require('../webpack.config');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 config.mode = 'production';
 config.output = {
@@ -9,19 +10,31 @@ config.output = {
   path: path.resolve(__dirname, '..', 'dist')
 };
 
-config.plugins = config.plugins.concat([
+config.optimization = {
+  minimizer: [new UglifyJsPlugin({
+    cache: true,
+    parallel: true,
+    uglifyOptions: {
+      compress: false,
+      ecma: 6,
+      mangle: true
+    },
+    sourceMap: true
+  })]
+};
+// config.plugins = config.plugins.concat([
 
-  // Reduces bundles total size
-  new webpack.optimize.UglifyJsPlugin({
-    mangle: {
+//   // Reduces bundles total size
+//   new webpack.optimize.UglifyJsPlugin({
+//     mangle: {
 
-      // You can specify all variables that should not be mangled.
-      // For example if your vendor dependency doesn't use modules
-      // and relies on global variables. Most of angular modules relies on
-      // angular global variable, so we should keep it unchanged
-      except: ['$super', '$', 'exports', 'require', 'angular']
-    }
-  })
-]);
+//       // You can specify all variables that should not be mangled.
+//       // For example if your vendor dependency doesn't use modules
+//       // and relies on global variables. Most of angular modules relies on
+//       // angular global variable, so we should keep it unchanged
+//       except: ['$super', '$', 'exports', 'require', 'angular']
+//     }
+//   })
+// ]);
 
 module.exports = config;
